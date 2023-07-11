c756d2f9f37d4ceb3071b2cb07408a7cf1935665
leo-Msb <117349958+leo-Msb@users.noreply.github.com>
2023-07-11 10:00:25 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const http = require("http");
-// const app = require('./app');
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
